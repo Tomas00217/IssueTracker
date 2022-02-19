684ba15de936570168af19eede917457335fa937
Tomas Stucka <xstucka@fi.muni.cz>
2022-02-19 18:59:02 +0100

    initial commit

---
+++ Models/IssueTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker.Models
+{
+    public class IssueTrackerContext : DbContext
+    {
+        public DbSet<Project> ?Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=D:\Projekty\IssueTracker\app.db");
+
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string? Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime TargetEndDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ActualEndDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedOn { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime ModifiedOn { get; set; }
+
+        public int ModifiedBy { get; set; }
+    }
+}
