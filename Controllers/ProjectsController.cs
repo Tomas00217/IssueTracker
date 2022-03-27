97cab5a183e2053969b943609a26cbd98784e67c
Tomas Stucka <xstucka@fi.muni.cz>
2022-03-27 20:26:53 +0200

    Added notifications to project

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,23 +1,21 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IssueTracker.Models;
 using IssueTracker.Utils;
+using AspNetCoreHero.ToastNotification.Abstractions;
 
 namespace IssueTracker.Controllers
 {
     public class ProjectsController : Controller
     {
         private readonly IssueTrackerContext _context;
+        private readonly INotyfService _notyf;
 
-        public ProjectsController(IssueTrackerContext context)
+        public ProjectsController(IssueTrackerContext context, INotyfService notyf)
         {
             _context = context;
+            _notyf = notyf;
         }
 
         private Boolean IsNotLogged()
@@ -34,7 +32,7 @@ namespace IssueTracker.Controllers
             {
                 return RedirectToAction("Index", "Authorization");
             }
-            // TODO ADD NOTYF
+            
             var userId = HttpContext.Session.GetInt32("UserId") ?? -1;
             var projects = _context.GetUserProjects(userId);
 
@@ -70,15 +68,44 @@ namespace IssueTracker.Controllers
             var projectIds = _context.PersonProjects.Select(proj => proj.ProjectId);
             ViewData["ProjectIds"] = projectIds;
 
-            var issues = _context.Issues.ToList();
-            ViewData["Issues"] = issues;
             string projectLead = _context.PersonProjects.Where(proj => proj.ProjectId == id && proj.Role == ProjectRole.ProjectLead).Select(p => p.Person.Email).SingleOrDefault();
             ViewData["ProjectLead"] = projectLead;
+
+            ViewData["Issues"] = _context.Issues.ToList();
             ViewData["UserRole"] = _context.PersonProjects.Where(proj => proj.ProjectId == id && proj.PersonId == userId).Select(pers => pers.Role).SingleOrDefault();
             ViewData["ProjectUsers"] = _context.PersonProjects.Where(proj => proj.ProjectId == id && proj.Role != ProjectRole.Manager).Select(pers => pers.Person.Email);
             ViewData["AllUsers"] = _context.Persons.Select(pers => pers.Email);
 
             return View(project);
+        }
+
+        public IActionResult InviteDeveloper(string email, int projectId)
+        {
+            int currentUserId = HttpContext.Session.GetInt32("UserId") ?? -1;
+            int userIdToAdd = _context.Persons.FirstOrDefault(pers => pers.Email == email).PersonId;
+
+            if (_context.PersonProjects.Where(pp => pp.PersonId == userIdToAdd && pp.ProjectId == projectId).Any())
+            {
+                _notyf.Error("User is already part of project");
+                return RedirectToAction("Details", new { id = projectId, userId = currentUserId });
+            }
+
+            PersonProject personProject = new()
+            {
+                PersonId = userIdToAdd,
+                ProjectId = projectId,
+                Role = ProjectRole.Developer
+            };
+
+            var project = _context.Projects.FirstOrDefault(proj => proj.ProjectId == projectId);
+
+            personProject.Project = project;
+            personProject.Person = _context.Persons.FirstOrDefault(x => x.PersonId == personProject.PersonId);
+            _context.PersonProjects.Add(personProject);
+            _context.SaveChanges();
+
+            _notyf.Success("User sucessfuly added");
+            return RedirectToAction("Details", new { id = projectId, userId = currentUserId });
         }
 
         // GET: Projects/Create
@@ -107,38 +134,19 @@ namespace IssueTracker.Controllers
 
             PersonProject personProject = new()
             {
-                PersonId = int.Parse(HttpContext.Session.GetString("UserId")),
+                PersonId = HttpContext.Session.GetInt32("UserId") ?? -1,
                 ProjectId = project.ProjectId,
                 Role = ProjectRole.Manager
             };
+
             personProject.Project = project;
             personProject.Person = _context.Persons.FirstOrDefault(x => x.PersonId == personProject.PersonId);
 
             _context.PersonProjects.Add(personProject);
             _context.SaveChanges();
 
+            _notyf.Success("Project sucessfuly created");
             return RedirectToAction("Index");
-        }
-
-        public IActionResult InviteDeveloper(string email, int projectId)
-        {
-            int userId = _context.Persons.FirstOrDefault(pers => pers.Email == email).PersonId;
-
-            PersonProject personProject = new()
-            {
-                PersonId = userId,
-                ProjectId = projectId,
-                Role = ProjectRole.Developer
-            };
-            // TODO ADD NOTYF ALSO DONT ADD ALREADY ADDED
-            var project = _context.Projects.FirstOrDefault(proj => proj.ProjectId == projectId);
-
-            personProject.Project = project;
-            personProject.Person = _context.Persons.FirstOrDefault(x => x.PersonId == personProject.PersonId);
-            _context.PersonProjects.Add(personProject);
-            _context.SaveChanges();
-
-            return RedirectToAction("Details");
         }
 
         // GET: Projects/Edit
@@ -168,7 +176,7 @@ namespace IssueTracker.Controllers
             }
             var projectToUpdate = await _context.Projects.FirstOrDefaultAsync(p => p.ProjectId == id);
             
-/*            if (await TryUpdateModelAsync<Project>(
+        /*  if (await TryUpdateModelAsync<Project>(
                 projectToUpdate,
                 "",
                 p => p.Name, p => p.StartDate, p => p.TargetEndDate, p => p.ActualEndDate, p => p.ModifiedBy))
@@ -189,7 +197,7 @@ namespace IssueTracker.Controllers
             return View(projectToUpdate);
         }
 
-        // GET: Projects/Delete/5
+        // GET: Projects/Delete
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -215,6 +223,8 @@ namespace IssueTracker.Controllers
             var project = await _context.Projects.FindAsync(id);
             _context.Projects.Remove(project);
             await _context.SaveChangesAsync();
+
+            _notyf.Success("Project sucessfuly deleted");
             return RedirectToAction(nameof(Index));
         }
 
